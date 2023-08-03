e4e7819c4eb05af07209e62e8b0fac7a5f80c1a7
azharmo22350 <azharmohammed22350@gmail.com>
2023-08-03 20:14:19 +0530

    complete task5

---
+++ Demo5.java
@@ -0,0 +1,10 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int part1 = 978;
+        int part2 = 3;
+        int part3 = 16;
+        int part4 = 1484100;
+
+        System.out.printf("\033[0;36m%03d\033[0m-\033[0;33m%d\033[0m-\033[0;35m%d\033[0m-\033[0;32m%d%n", part1, part2, part3, part4);
+    }
+}
