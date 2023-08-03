240b5654a9fdc7e8b791e55970e9025ae44ca2cb
azharmo22350 <azharmohammed22350@gmail.com>
2023-08-03 20:16:20 +0530

    complete taskc

---
+++ Demo6_task_c.java
@@ -0,0 +1,13 @@
+public class Demo6_task_c {
+    public static void main(String[] args) {
+
+        System.out.println("\033[0;33m+-----------------+----------------+---------------+");
+        System.out.println("| \033[0;1mCITY\033[0;33m            |  \033[0;1mSTATE\033[0;33m         |  \033[0;1mPOPULATION\033[0;33m   |");
+        System.out.println("\033[0;33m+-----------------+----------------+---------------+");
+        System.out.println("| \033[0;0mLos Angeles\033[0;33m     |  \033[0;0mCalifornia\033[0;33m    |  \033[0;35m3,966,936\033[0;33m    |");
+        System.out.println("| \033[0;32mNew \033[0mYork\033[0;33m        |  \033[0;32mNew \033[0mYork\033[0;33m      |  \033[0;35m8,336,817\033[0;33m    |");
+        System.out.println("\033[0;33m+-----------------+----------------+---------------+");
+        
+    }
+    
+}
