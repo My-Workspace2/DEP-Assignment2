dee6ea0ef8ead36178285272aab8978ffcba72b8
azharmo22350 <azharmohammed22350@gmail.com>
2023-08-03 20:15:48 +0530

    complete taskb

---
+++ Demo6_task_b.java
@@ -0,0 +1,11 @@
+public class Demo6_task_b {
+    public static void main(String[] args) {
+        System.out.println("\033[0;33m+------------------+----------+");
+        System.out.println("| \033[34;1mNAME\033[0;33m             |  \033[34;1mAGE\033[0;33m     |");
+        System.out.println("+------------------+----------+");
+        System.out.println("| \033[0;30mAlice\033[0;33m            |   \033[0;35m24\033[0;33m     |");
+        System.out.println("| \033[0;30mBob\033[0;33m              |   \033[0;35m30\033[0;33m     |");
+        System.out.println("\033[0;33m+------------------+----------+");
+    }
+    
+}
